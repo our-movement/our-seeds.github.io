90ce1f59749dada853a7212affa4f92cf91dab96
leifriksheim <riksheim.leif@gmail.com>
2020-05-26 15:33:23 -0600

    Updates

---
+++ precache-manifest.e553947e98d7b53f2938422361ba908c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496ed885b33edd6965ecdcfd3368f8c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcd767da6bbeeedec285",
+    "url": "/static/css/2.3191a921.chunk.css"
+  },
+  {
+    "revision": "0696b9a8a175d33ae96e",
+    "url": "/static/css/main.4b1c1b7b.chunk.css"
+  },
+  {
+    "revision": "bcd767da6bbeeedec285",
+    "url": "/static/js/2.6ec1e2df.chunk.js"
+  },
+  {
+    "revision": "aacc91c2547fe242b2208c0cb5f420e7",
+    "url": "/static/js/2.6ec1e2df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0696b9a8a175d33ae96e",
+    "url": "/static/js/main.11e69db6.chunk.js"
+  },
+  {
+    "revision": "5a73259064fe15d3f24e8a1f8345efaf",
+    "url": "/static/js/main.11e69db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92273b49d3690110aa39",
+    "url": "/static/js/runtime-main.584e3335.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ddbc47b658906474df2c6118077d2f6e.js"
+  "/precache-manifest.e553947e98d7b53f2938422361ba908c.js"
 );
 
 self.addEventListener('message', (event) => {
